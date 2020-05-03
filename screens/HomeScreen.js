62da24ff4213ce29fe4072fd9db6a5bd294b0e16
Alex Philayvanh <aphilay@gmail.com>
2020-05-03 06:52:26 -0700

    performed crud operations with firebase realtime db

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,52 @@
 import React from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
+import { db } from "../config";
+const HomeScreen = ({ navigation }) => {
+  //adding name in the names collection
+  addName = (data) => {
+    let key = db.database().ref("/names").push().key;
+    db.database()
+      .ref("/names")
+      .child(key)
+      .set({ firstname: "corey", lastname: "nguyen" });
+  };
 
-const HomeScreen = ({ navigation, props }) => {
-  console.log(props);
+  //adding color in the colors collection
+  addColor = () => {
+    db.database().ref("/colors").push().set({
+      first: "green",
+      second: "orange",
+    });
+  };
+
+  //read a color given the key
+  readColor = () => {
+    db.database()
+      .ref("colors/-M6PgcAH72ic0L7XKOQL")
+      .on("value", (snap) => {
+        console.log(snap.val());
+      });
+  };
+
+  //update a color in the colors collection given a key
+  updateColor = () => {
+    db.database()
+      .ref("colors/" + "-M6PgcAH72ic0L7XKOQL")
+      //.push()
+      .update({
+        first: "black",
+        second: "orange",
+      });
+  };
+
+  deleteColor = () => {
+    //delete a child e.g. -M6Pe_dG_hoIKUf2jO6y
+    db.database()
+      .ref("colors/" + "-M6PgdDCy07d4W-m8Ml9")
+      .remove();
+    //setting to null is acceptable
+    //.set(null);
+  };
   return (
     <View style={styles.center}>
       <Text>Hello, </Text>
@@ -15,6 +59,12 @@ const HomeScreen = ({ navigation, props }) => {
         title="Go to Settings"
         onPress={() => navigation.navigate("Settings")}
       />
+      <Button title="Add a name" onPress={() => this.addName()} />
+
+      <Button title="Create a color" onPress={() => this.addColor()} />
+      <Button title="Read a color" onPress={() => this.readColor()} />
+      <Button title="Update a color" onPress={() => this.updateColor()} />
+      <Button title="Delete a color" onPress={() => this.deleteColor()} />
     </View>
   );
 };
